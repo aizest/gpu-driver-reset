4bdffd01c40e9b37009c770d4e2dc1177e6eba5a
aizest <mail2xz@hotmail.com>
2021-10-27 18:01:27 -0500

    Add checking period (every 0.5 hr) and logging logic

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import subprocess
+import time
+import logging
 
-def run_command():
-    # result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE)
-    # try:
-    #     result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #     print(type(result.stdout.decode('utf-8')))
-    # except subprocess.CalledProcessError as e:
-    #     result = e.output
-    #     print(result)
+logging.basicConfig(level=logging.INFO, filename='reset-gpu.log', format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info("server started!")
+
+
+def check_gpu_driver():
     result = subprocess.Popen("nvidia-smi", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, err = result.communicate()
     if output:
-        print('GPU driver runs properly! No need to do anything.\n' + output.decode("utf-8"))
+        # print('GPU driver runs properly! No need to do anything.\n' + output.decode("utf-8"))
+        logging.info('GPU driver runs properly! No need to do anything.')
     else:
         err_msg = err.decode("utf-8")
-        print('GPU driver fails!\n' + err_msg)
+        # print('GPU driver fails!\n' + err_msg)
+        logging.error('GPU driver fails!\n' + err_msg)
         if 'version mismatch' in err_msg:
-            print('Trying to reset GPU driver...')
+            # print('Trying to reset GPU driver...')
+            logging.info('Trying to reset GPU driver...')
             output_reset, err_reset = subprocess.Popen("nvidia-smi", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if output_reset:
-                print('GPU driver reset was successful!\n' + output_reset.decode('utf-8'))
+                # print('GPU driver reset was successful!\n' + output_reset.decode('utf-8'))
+                logging.info('GPU driver reset was successful!\n' + output_reset.decode('utf-8'))
             else:
-                print('Failed to reset GPU driver. \n' + err_reset.decode('utf-8'))
+                # print('Failed to reset GPU driver. \n' + err_reset.decode('utf-8'))
+                logging.error('Failed to reset GPU driver. \n' + err_reset.decode('utf-8'))
 
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    run_command()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        check_gpu_driver()
+        time.sleep(1800)
